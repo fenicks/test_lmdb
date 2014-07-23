43f7239788093d74f7e49126f7f43dabe256f9a7
Christian Kakesa <christian.kakesa@gmail.com>
2014-07-23 15:28:46 +0200

    Test MDB return value with MDB_SUCCESS.

--- test_lmdb.c
+++ test_lmdb.c
@@ -18,13 +18,13 @@ int main(int ac, char **ag) {
   /* MDB environnment */
   MDB_env *env;
   err = mdb_env_create(&env);
-  if (err != 0) {
+  if (err != MDB_SUCCESS) {
     fprintf(stderr, "%s\n", mdb_strerror(err));
     return -42;
   }
 
   err = mdb_env_open(env, store_dir, 0, 0640);
-  if (err != 0) {
+  if (err != MDB_SUCCESS) {
     fprintf(stderr, "Can't open the data store '%s': %s\n", store_dir, mdb_strerror(err));
     mdb_env_close(env); /* As mentioned in the documentation */
     return -42;
@@ -33,7 +33,7 @@ int main(int ac, char **ag) {
   /* Handling transaction */
   MDB_txn *txn;
   err = mdb_txn_begin(env, NULL, 0, &txn);
-  if (err != 0) {
+  if (err != MDB_SUCCESS) {
     fprintf(stderr, "Can't begin a transaction: %s\n", mdb_strerror(err));
     mdb_env_close(env);
     return -42;
@@ -42,7 +42,7 @@ int main(int ac, char **ag) {
   /* Handle databases in environment */
   MDB_dbi dbi;
   err = mdb_dbi_open(txn, NULL, 0, &dbi);
-  if (err != 0) {
+  if (err != MDB_SUCCESS) {
     fprintf(stderr, "Can't open a database: %s\n", mdb_strerror(err));
   }
 
@@ -58,18 +58,18 @@ int main(int ac, char **ag) {
   value.mv_data = sval;
 
   err = mdb_put(txn, dbi, &key, &value, 0);
-  if (err != 0) {
+  if (err != MDB_SUCCESS) {
     fprintf(stderr, "Can't add a key/value to the database: %s\n", mdb_strerror(err));
   }
 
   /* Navigation into the database */
   MDB_cursor *cursor;
   err = mdb_cursor_open(txn, dbi, &cursor);
-  if (err != 0) {
+  if (err != MDB_SUCCESS) {
     fprintf(stderr, "Can't open a cursor to the database: %s\n", mdb_strerror(err));
   }
   MDB_val ret_k, ret_v;
-  while ((err = mdb_cursor_get(cursor, &ret_k, &ret_v, MDB_NEXT)) == 0) {
+  while ((err = mdb_cursor_get(cursor, &ret_k, &ret_v, MDB_NEXT)) == MDB_SUCCESS) {
     printf("key: %s - val: %s\n", ret_k.mv_data, ret_v.mv_data);
   }
 
